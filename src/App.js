92658317758cab6e306fbd4a7a5e8df22147383d
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2021-08-29 17:43:10 -0500

    simple dropdown

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import './App.css';
+import { useState } from 'react';
+import { Dropdown } from './Dropdown';
 
+const options = [
+  {value: 'goldfish', label:'GoldFish'},
+  {value:"dog", label:"Dog"},
+  {value:"cat", label:"Cat"},
+  {value:"hamster", label:"Hamster"},
+  {value:"parrot", label:"Parrot"},
+  {value:"spider", label:"Spider"},
+  {value:"goldfish", label:"Goldfish"}
+]
+const initialValue = 'hamster'
 function App() {
+  const [selectedValue, setSelectedValue] = useState(initialValue)
+
   return (
-    <div></div>    
+    <div>
+      <label for="pet-select">Choose a pet:</label>
+
+      <Dropdown 
+        options={options}
+        id='pet-select'
+        selectedValue={selectedValue}
+        onSelectedValueChane={setSelectedValue}
+      />
+
+    </div>    
   );
 }
 
